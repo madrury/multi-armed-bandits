9c9cfb6f715e0205e5098ec55199ff7870ca7f8a
Matthew Drury <matthew.drury.83@gmail.com>
2018-12-24 12:42:15 -0800

    Fix include bug in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from codecs import open
 from os import path
 
@@ -18,8 +18,11 @@ setup(
     url='https://github.com/madrury/multi-armed-bandits',
     author='Matthew Drury',
     author_email='matthew.drury.83@gmail.com',
-    ext_modules=cythonize("mab/bandits.pyx", annotate=True),
-    include_dirs=[np.get_include()],
+    ext_modules=cythonize(Extension(
+        "mab",
+        sources=["mab/bandits.pyx"], 
+        annotate=True,
+        include_dirs=[np.get_include()])),
     packages=['mab'],
     install_requires=['numpy', 'cython']
 )
